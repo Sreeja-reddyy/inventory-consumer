c28a4e7de3ca80c97c7e4c800a64d13e234fe009
Sree <padidemsreejareddy4881@gmail.com>
2022-05-10 13:13:06 +0530

    Inventory consumer 1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from './authentication.service';
+
+import { ProductService } from './product.service';
+import { Product } from './product';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'inventory-consumer';
+  title = 'Product Inventory Management System';
+  imagePath='/assets/images/ims.jpg';
+
+  constructor(public loginService:AuthenticationService) { }
 }
